1d9c62eef877bbbcff11b17f8dc5ae40133ce40d
barbara.tavares <barbara.tavares@unicred.com.br>
2025-02-25 23:27:00 -0300

    desafios

--- src/EncerraPrograma.java
+++ src/EncerraPrograma.java
@@ -1,19 +1,22 @@
-
 import java.util.Scanner;
 
 public class EncerraPrograma {
     public static void main(String[] args) {
-        int numero = 0;
         Scanner scanner = new Scanner(System.in);
+        int numero;
 
-        while (numero != 10) {
-            System.out.print("Digite um valor: ");
+        do {
+            System.out.print("Digite um valor entre 1 e 10: ");
             numero = scanner.nextInt();
-            scanner.nextLine();
 
-            if (numero == 10) {
+            if (numero < 1 || numero > 10) {
+                System.out.println("número inválido.");
+            } else if (numero == 10) {
                 System.out.println("Você digitou o valor correto: " + numero);
+                System.out.println("Programa encerrado.");
+            } else {
+                System.out.println("Você digitou: " + numero);
             }
-        }
+        } while (numero != 10);
     }
 }
